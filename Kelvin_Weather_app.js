95884d4b2b291b774765eb75521fcf8801bd8238
paulgureghian <paulgureghian@att.net>
2020-04-14 15:53:08 -0700

    Declared variables

---
+++ Kelvin_Weather_app.js
@@ -0,0 +1,16 @@
+// Created by Paul A. Gureghian in April 2020 //
+// This JS app converts Klevin to Celsius then to Fahrenheit //
+
+// Declare variables 
+// Declare a constant variable for Kelvin 
+const kelvin = 293;
+
+// Declare a constant variable for converting Kelvin to Celsius  
+const celsius = kelvin - 273;  
+
+// Declare a flexible variable for converting Celsius to Fahrenheit
+let fahrenheit = celsius * (9/5) + 32;
+
+// Round down Fahrenheit after conversion from Celsius  
+fahrenheit = Math.floor(fahrenheit);  
+    
