27cdf1686d0d23d49ee11a3227dc796850dbca17
paulgureghian <paulgureghian@att.net>
2020-04-14 18:53:01 -0700

    Added a code comment

--- Kelvin_Weather_app.js
+++ Kelvin_Weather_app.js
@@ -18,8 +18,13 @@ fahrenheit = Math.floor(fahrenheit);
 console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
 
 //  Convert Celsius to Newton
+// Declare a flexible variable for converting Celsius to Newton
 let newton = celsius * (33/100);
+
+// Round down Newton after conversion from Celsius 
 newton = Math.floor(newton);
+
+// Print the temperature in Newton
 console.log(`The temperature is ${newton} degrees Newton.`); 
 
 // End of script
