36d45b0d3d42256e629e00bb01270efedd150dca
paulgureghian <paulgureghian@att.net>
2020-04-15 16:41:15 -0700

    Declared variables

---
+++ Dog_Years.js
@@ -0,0 +1,11 @@
+// Created by Paul A. Gureghian in April 2020 //
+// This JS app converts human age into dog years //
+
+// Declare variables
+// Declare a constant variable for my human age
+const myAge = 55;
+
+// Declare a flexible variable for early years
+let earlyYears = 2;
+
+
