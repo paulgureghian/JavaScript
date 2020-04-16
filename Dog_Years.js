1b39bac10f09cf53d7e6c0fce295ada75aad3a9c
paulgureghian <paulgureghian@att.net>
2020-04-15 18:01:22 -0700

    Used subtraction operator

--- Dog_Years.js
+++ Dog_Years.js
@@ -2,13 +2,16 @@
 // This JS app converts human age into dog years //
 
 // Declare variables
-// Declare a constant variable for my human age
+// Declare a constant variable for my human age 
 const myAge = 55;
 
 // Declare a flexible variable for early years
 let earlyYears = 2;
 
-// Multiply 'early years' by 10.5 //
+// Multiply 'early years' by 10.5 
 earlyYears *= 10.5;
+
+// Subtract 2 from 'myAge' 
+let laterYears = myAge - 2;
 
 
