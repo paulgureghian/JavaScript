9e7103c4d1d0710b9bb13f2a1318277aedb6beed
paulgureghian <paulgureghian@att.net>
2020-04-16 19:17:46 -0700

    Added a switch statement

--- Magic_Eight_Ball.js
+++ Magic_Eight_Ball.js
@@ -22,3 +22,31 @@ let randomNumber = Math.floor(Math.random() * 8);
 // Declare a flexible variable for 'eightBall'
 let eightBall = '';
 
+// Create a control flow 
+switch (randomNumber) {
+    case 0:
+        eightBall = 'It is certain';
+        break;
+    case 1: 
+        eightBall = 'It is decidedly so';
+        break;
+    case 2:
+        eightBall = 'Reply hazy try again';
+        break;
+    case 3: 
+        eightBall = "Cannot predict now";
+        break;
+    case 4: 
+        eightBall = "Do not count on it";
+        break;
+    case 5: 
+        eightBall = 'My sources say no';
+        break;
+    case 6: 
+        eightBall = 'Outlook not so good';
+        break;
+    case 7:
+        eightBall = 'Signs point to yes';
+        break;                             
+}
+
