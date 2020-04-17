a561ffd6180e04c987b96a15eaafe65a9541c3b1
paulgureghian <paulgureghian@att.net>
2020-04-17 16:50:06 -0700

    Fixed ESLint errors

--- Kelvin_Weather_app.js
+++ Kelvin_Weather_app.js
@@ -9,7 +9,7 @@ const kelvin = 293;
 const celsius = kelvin - 273;  
 
 // Declare a flexible variable for converting Celsius to Fahrenheit
-let fahrenheit = celsius * (9/5) + 32;
+let fahrenheit = celsius * (9 / 5) + 32;
 
 // Round down Fahrenheit after conversion from Celsius  
 fahrenheit = Math.floor(fahrenheit);  
@@ -19,7 +19,7 @@ console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
 
 // Convert Celsius to Newton
 // Declare a flexible variable for converting Celsius to Newton
-let newton = celsius * (33/100);
+let newton = celsius * (33 / 100);
 
 // Round down Newton after conversion from Celsius 
 newton = Math.floor(newton);
@@ -27,4 +27,4 @@ newton = Math.floor(newton);
 // Print the temperature in Newton
 console.log(`The temperature is ${newton} degrees Newton.`); 
 
-// End of script
+// End of script //
