fd7ff41a8947b5bf6e477eda85ac824d01b4ac97
paulgureghian <paulgureghian@att.net>
2020-04-17 16:40:26 -0700

    Added Hello_World.js

---
+++ Hello_World.js
@@ -0,0 +1,12 @@
+// Created by Paul A. Gureghian in April 2020 //
+// This JS project prints "Hello, World" in the console //
+
+function greeting() {
+    var inputString = readLine();
+
+    console.log("Hello, World");
+
+    console.log(inputString);   
+}
+
+greeting()
