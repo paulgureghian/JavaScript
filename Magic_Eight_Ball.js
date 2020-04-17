300e985fdb27ed619ae6f73bfc32504160d42eb9
paulgureghian <paulgureghian@att.net>
2020-04-16 18:11:54 -0700

    Declared a variable

---
+++ Magic_Eight_Ball.js
@@ -0,0 +1,9 @@
+// Created by Paul A. Gureghian in April 2020 //
+/* This JS project app uses the key concept of 'Control Flow' 
+   to input a question and output a random fortune. */
+
+// Declare variables 
+// Declare a flexible variable for 'userName' 
+let userName = 'Paul';
+
+// Create a ternary expression for 'userName'
