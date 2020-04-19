d39bc07fcfcb5a5c7cb2c1f2f4aed922cd12e61e
paulgureghian <paulgureghian@att.net>
2020-04-18 19:51:04 -0700

    Declared a variable

--- Race_Day.js
+++ Race_Day.js
@@ -1,5 +1,8 @@
 // Created by Paul A. Gureghian in April 2020 //
 // This JS project app registers runners for Codeacadmy's annual race //
 // And gives them instructions on race day //
+// Key concepts covered here are 'Control Flow & If/else Statements
 
-// 
+// Declare a flexible variable for 'raceNumber'
+let raceNumber = Math.floor(Math.random() * 1000);
+
