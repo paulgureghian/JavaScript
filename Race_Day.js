ca50221ee68dafb29fac607e3e0b0847a5a2f9eb
paulgureghian <paulgureghian@att.net>
2020-04-19 18:04:10 -0700

    Added a control flow statement

--- Race_Day.js
+++ Race_Day.js
@@ -28,4 +28,6 @@ else if(!runnerRegistered && runnersAge > 18) {
 else if(runnersAge < 18){
     console.log(`Your race time is 12:30 pm, and your race number is: ${raceNumber}.`);
 } 
-
+else{
+    console.log('Please see the registration desk');
+}
