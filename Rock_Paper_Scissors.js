6bf1e855a80cc61bff6d561e7cab71f6f44e1e88
paulgureghian <paulgureghian@att.net>
2020-04-20 18:28:36 -0700

    Defined a function

--- Rock_Paper_Scissors.js
+++ Rock_Paper_Scissors.js
@@ -1,3 +1,7 @@
 // Created by Paul A. Gureghian in April 2020 //
-// This JS project app runs the classic two player game of Rock, Paper, Scissors
+// This JS project app runs the classic two player game of Rock, Paper, Scissors //
 
+// Declare a 'const' function for user choice
+const getUserChoice = (userInput) => {
+    
+}
