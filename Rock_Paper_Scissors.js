fe068bcc824f25a16deec8c1e2451c6ae80a79d3
paulgureghian <paulgureghian@att.net>
2020-04-21 15:54:59 -0700

    Added a switch statement

--- Rock_Paper_Scissors.js
+++ Rock_Paper_Scissors.js
@@ -18,5 +18,13 @@ const getUserChoice = (userInput) => {
 const getComputerChoice = () => {
 
     const randomNumber = Math.floor(Math.random() * 3);
-
+    
+    switch (randomNumber) {
+        case 0: 
+        return 'rock';
+        case 1:
+            return 'paper';
+            case 2: 
+            return 'scissors';
+    }
 };
