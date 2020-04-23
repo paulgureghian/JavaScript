2822d44b44d4c4cb9761559c211dccde35664c5a
paulgureghian <paulgureghian@att.net>
2020-04-23 15:37:37 -0700

    Added if/else statements

--- Sleep_Debt_Calculator.js
+++ Sleep_Debt_Calculator.js
@@ -3,5 +3,33 @@
 
 // Define a function which calculates the amount of sleep in any given night.
 const getSleepHours = (day) => {
-    
-}
+    if (day === 'monday') {
+        return 8;
+    } 
+    else if (day === 'tuesday') {
+        return 7;
+    }
+    else if (day === 'wednesday') {
+        return 8;
+    }
+
+    else if (day === 'thursday') {
+        return 7;
+    }
+
+    else if (day === 'friday') {
+        return 7;
+    }
+
+    else if (day === 'saturday') {
+        return 6;
+    }
+
+    else if (day === 'sunday') {
+        return 6;
+    }
+    else {
+        return 'Error';
+    }
+
+};
