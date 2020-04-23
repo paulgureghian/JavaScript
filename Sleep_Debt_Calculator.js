d8c288f09c1418d6ee3151df07eb9f233ed85519
paulgureghian <paulgureghian@att.net>
2020-04-23 14:05:02 -0700

    Created JS project app file

---
+++ Sleep_Debt_Calculator.js
@@ -0,0 +1,4 @@
+// Created by Paul A. Gureghian in April 2020 //
+// This JS project app calculates the amount of sleep you get each week //
+
+// Define a function which calculates the amount of sleep in any given night.
