5badbc7474df1e740a0226a5a53fd1e5b3474821
paulgureghian <paulgureghian@att.net>
2020-04-23 15:40:33 -0700

    Fixed linter errors"

--- Sleep_Debt_Calculator.js
+++ Sleep_Debt_Calculator.js
@@ -3,33 +3,38 @@
 
 // Define a function which calculates the amount of sleep in any given night.
 const getSleepHours = (day) => {
-    if (day === 'monday') {
+
+    if (day === "monday") {
+
         return 8;
-    } 
-    else if (day === 'tuesday') {
+    
+    } else if (day === "tuesday") {
+
         return 7;
-    }
-    else if (day === 'wednesday') {
+    
+    } else if (day === "wednesday") {
+
         return 8;
-    }
+    
+    } else if (day === "thursday") {
 
-    else if (day === 'thursday') {
         return 7;
-    }
+    
+    } else if (day === "friday") {
 
-    else if (day === 'friday') {
         return 7;
-    }
+    
+    } else if (day === "saturday") {
 
-    else if (day === 'saturday') {
         return 6;
-    }
+    
+    } else if (day === "sunday") {
 
-    else if (day === 'sunday') {
         return 6;
+    
     }
-    else {
-        return 'Error';
-    }
+    
+    return "Error";
+    
 
 };
