8ea7fd348b036516a243c65c5ebb62cf743beb18
paulgureghian <paulgureghian@att.net>
2020-04-23 14:09:22 -0700

    Defined a function

--- Sleep_Debt_Calculator.js
+++ Sleep_Debt_Calculator.js
@@ -2,3 +2,6 @@
 // This JS project app calculates the amount of sleep you get each week //
 
 // Define a function which calculates the amount of sleep in any given night.
+const getSleepHours = (day) => {
+    
+}
