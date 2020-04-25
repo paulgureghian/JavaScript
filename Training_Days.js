ca7d3a7002e6502df43eb15bc2c00f08ba6fb1d4
paulgureghian <paulgureghian@att.net>
2020-04-25 15:19:33 -0700

    Added if/else statements

--- Training_Days.js
+++ Training_Days.js
@@ -1,6 +1,7 @@
 // Created by Paul A. Gureghian in April 2020 //
 // This JS projct app sends a message about Training Days events //
 
+// The scope of 'random' is too loose
 const random = Math.floor(Math.random() * 3);
 
 const getRandEvent = () => {
@@ -13,6 +14,16 @@ const getRandEvent = () => {
     }
 };
 
-const getTrainingDays = () => {
+// The scope of 'days' is too tight
+const getTrainingDays = event => {
+    if(event === 'Marathon') {
+        let days = 50;
+    } else if (event === 'Triathlon') {
+        let days = 100;
+    } else if (event === 'Pentathlon') {
+        let days = 200;
+    }
+
+    return days;
     
-}
+};
