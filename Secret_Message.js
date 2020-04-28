f41b413161868ab34156456ae2d4d42b98fd86e4
paulgureghian <paulgureghian@att.net>
2020-04-28 13:26:12 -0700

    Added a console.log statement

--- Secret_Message.js
+++ Secret_Message.js
@@ -28,3 +28,5 @@ console.log("The length of the array after elements are added is:", secretMessag
 secretMessage[7] ='right';
 
 // Print the array after an element is changed. //
+console.log("The new array after the element is changed is:", secretMessage);
+
