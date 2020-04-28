bac05c4aaed1e61db95d956c56c7448bd7cdd5ac
paulgureghian <paulgureghian@att.net>
2020-04-27 18:19:33 -0700

    Added a code comment

--- Secret_Message.js
+++ Secret_Message.js
@@ -6,4 +6,5 @@ let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get',
                      'easily', 'the', 'first', 'time', 'it', 'is', 'about', 'what',
                      'you', 'can', 'figure', 'out', '-2015', 'Chris', 'Pine', 'Learn', 'JavaScript'];
                     
- 
+// Remove the last string element with an array method. //
+  
