e497165fe095e04cf968c886a6040a3d95ee5587
paulgureghian <paulgureghian@att.net>
2020-04-27 17:24:33 -0700

    Defined a string array

--- Secret_Message.js
+++ Secret_Message.js
@@ -1,5 +1,10 @@
 // Created by Paul A. Gureghian in April 2020. //
 // This JS project app uses array methods to transform an array of strings into a secret message. //
 
-
+// Define an array of strings. //
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get',
+                     'easily', 'the', 'first', 'time', 'it', 'is', 'about', 'what',
+                     'you', 'can', 'figure', 'out', '-2015', 'Chris', 'Pine', 'Learn', 'JavaScript'];
+                    
+                    
  
