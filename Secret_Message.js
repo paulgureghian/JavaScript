590afefdf7b5b4e6e8f9fcb13f934441074627d0
paulgureghian <paulgureghian@att.net>
2020-04-27 18:39:01 -0700

    Added a console.log statement

--- Secret_Message.js
+++ Secret_Message.js
@@ -6,11 +6,14 @@ let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get',
                      'easily', 'the', 'first', 'time', 'it', 'is', 'about', 'what',
                      'you', 'can', 'figure', 'out', '-2015', 'Chris', 'Pine', 'Learn', 'JavaScript'];
 
-// Print the array. //                     
+// Print the array and length. //                     
 console.log("The array:", secretMessage, "\n");                     
+console.log("The length of the original array:", secretMessage, "\n");
 
 // Remove the last string element with an array method. //
 secretMessage.pop();
 
-// Print the array. //
+// Print the array and length. //
 console.log("The array after last element removed:", secretMessage, "\n");
+console.log("The length of the array after element removed:", secretMessage, "\n");
+
