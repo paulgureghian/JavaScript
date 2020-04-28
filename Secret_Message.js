0c730bb39d6f65a83b26c2cb4da1173e3e3df4d1
paulgureghian <paulgureghian@att.net>
2020-04-27 18:09:50 -0700

    Made formatting changes

--- Secret_Message.js
+++ Secret_Message.js
@@ -6,5 +6,4 @@ let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get',
                      'easily', 'the', 'first', 'time', 'it', 'is', 'about', 'what',
                      'you', 'can', 'figure', 'out', '-2015', 'Chris', 'Pine', 'Learn', 'JavaScript'];
                     
-                    
  
