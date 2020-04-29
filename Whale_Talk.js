3997753aea27324870151d9c757b6aa0915bb22c
paulgureghian <paulgureghian@att.net>
2020-04-29 12:18:25 -0700

    Added a code comment

--- Whale_Talk.js
+++ Whale_Talk.js
@@ -1,5 +1,8 @@
 // Created by Paul A. Gureghian in April 2020. //
 // This JS project app translates phrases into their "Whale Talk" equivalent. //
+// Key concept used in this script app is 'For Loops'. //
 
 // Start of script. //
+
+// Declare a variable to store the input phrase. //
 
