5ffd74a1e17b75b6284b3f406daa1921b2c4f0bc
paulgureghian <paulgureghian@att.net>
2020-05-18 16:02:22 -0700

    Created a JavaScript program file

---
+++ Meal_Maker.js
@@ -0,0 +1,6 @@
+// Created by Paul A. Gureghian in May 2020. //
+// This JavaScript program creates a three-course meal //
+// Based upon what is available on a menu. //
+
+// Start of program. //
+
