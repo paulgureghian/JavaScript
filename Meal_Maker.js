921765b80d86453a79028524e22a4061a4e0f4f6
paulgureghian <paulgureghian@att.net>
2020-05-19 14:25:26 -0700

    Added getter methods

--- Meal_Maker.js
+++ Meal_Maker.js
@@ -14,4 +14,17 @@ const menu = {
         desserts: [],
     },
 
+    get appetizers() {
+        return this._courses.appetizers;
+    },
+
+    get mains() {
+        return this._courses.mains;
+    },
+
+    get desserts() {
+        return this._courses.desserts;
+    },
+
+
 };
