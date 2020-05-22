588e0a3fb6d14dbc1ca035da2f17c55de67a4b3d
paulgureghian <paulgureghian@att.net>
2020-05-21 17:08:56 -0700

    Added getter methods for the keys

--- Team_Stats.js
+++ Team_Stats.js
@@ -19,6 +19,12 @@ const team = {
     ],
 
     // Create getter methods for the keys. //
-    
+    get() {
+        return this._players;
+    },
+
+    get() {
+        return this._games;
+    }
 
 };
