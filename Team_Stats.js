b685ff444338bd4c93d487a203574f0721a554ca
paulgureghian <paulgureghian@att.net>
2020-05-23 17:26:46 -0700

    Fixed linter errors

--- Team_Stats.js
+++ Team_Stats.js
@@ -7,33 +7,46 @@
 const team = {
 
     // Add object properties. //
-    _players: [
-        {firstName: "James", lastName: "Worthy", age: 25},
-        {firstName: "Kobe", lastName: "Bryant", age: 30},
-        {firstName: "Jerry", lastName: "West", age:32}
+    "_players": [
+        {"firstName": "James",
+"lastName": "Worthy",
+"age": 25},
+        {"firstName": "Kobe",
+"lastName": "Bryant",
+"age": 30},
+        {"firstName": "Jerry",
+"lastName": "West",
+"age": 32}
     ],
-    _games: [
-        {opponent: "Celtics", teamPoints: 120, opponentPoints: 133},
-        {opponent: "Bucks", teamPoints :122, opponentPoints: 134},
-        {opponent: "Knicks", teamPoints: 150, opponentPoints: 144}    
+    "_games": [
+        {"opponent": "Celtics",
+"teamPoints": 120,
+"opponentPoints": 133},
+        {"opponent": "Bucks",
+"teamPoints": 122,
+"opponentPoints": 134},
+        {"opponent": "Knicks",
+"teamPoints": 150,
+"opponentPoints": 144}    
     ],
 
     // Create getter methods for the keys. //
-    get players() {
+    get "players"() {
         return this._players;
     },
 
-    get games() {
+    get "games"() {
         return this._games;
     },
 
     // Add an 'addPlayer' method. ///
     addPlayer(firstName, lastName, age) {
        let player = {
-           firstName: firstName,
-           lastName: lastName,
-           age: age
+           firstName,
+           lastName,
+           age
        };
+
        this.players.push(player);
     }
 };
