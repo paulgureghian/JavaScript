1eb4df6707888badf519a62e3c33ea5d3dc1a01d
paulgureghian <paulgureghian@att.net>
2020-05-30 16:51:14 -0700

    Style formatted

--- Library.js
+++ Library.js
@@ -7,24 +7,24 @@
 
 class Media {
 
-    constructor(title) {
-        this._title = title;
-        this._isCheckedOut = false;
-        this._ratings = [];
-    }
+  constructor(title) {
+    this._title = title;
+    this._isCheckedOut = false;
+    this._ratings = [];
+  }
 
-    // Create getters. //
-    get title() {
-      return this._title; 
-    } 
+  // Create getters. //
+  get title() {
+    return this._title; 
+  } 
 
-    get isCheckedOut() {
-      return this._isCheckedOut;
-    }
+  get isCheckedOut() {
+    return this._isCheckedOut;
+  }
 
-    get ratings() {
-      return this._ratings;
-    }
+  get ratings() {
+    return this._ratings;
+  }
 
 
 
