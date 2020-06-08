2f3c2b4463395ab3aea4cee76e26f7610c0d9628
paulgureghian <paulgureghian@att.net>
2020-06-08 15:43:51 -0700

    Created a JS program file

---
+++ Transpilation.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+// Created by Paul A. Gureghian in June 2020. //
+// This JavaScript program demos the setting up a JS dir with Babel to transpile ES6+ code to ES5. //     
+
+// Start of program. //
